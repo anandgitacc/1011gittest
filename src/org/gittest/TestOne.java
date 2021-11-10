39c95dfa60ecb078ef0954d3d140f238ded2267f
anandgitacc <anand.akmail@gmail.com>
2021-11-10 14:47:22 +0530

    gittest

---
+++ src/org/gittest/TestOne.java
@@ -0,0 +1,15 @@
+package org.gittest;
+
+public class TestOne {
+	
+	private void empName() {
+		System.out.println("Anand");
+	}
+	
+	public static void main(String[] args) {
+		
+		TestOne t = new TestOne();
+		t.empName();
+	}
+
+}
